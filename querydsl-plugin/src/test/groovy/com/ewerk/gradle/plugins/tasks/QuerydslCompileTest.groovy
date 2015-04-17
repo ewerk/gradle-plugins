9dd4c4af3eaa360011f2b58c2f8a854aa2c239f4
Illya Boyko <illya.boyko@gmail.com>
2015-04-17 07:31:30 +0200

    Lic added, method name corrected

--- querydsl-plugin/src/test/groovy/com/ewerk/gradle/plugins/tasks/QuerydslCompileTest.groovy
+++ querydsl-plugin/src/test/groovy/com/ewerk/gradle/plugins/tasks/QuerydslCompileTest.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ewerk.gradle.plugins.tasks;
 
 import static org.hamcrest.CoreMatchers.hasItem
@@ -11,11 +26,14 @@ import org.junit.Test
 
 import com.ewerk.gradle.plugins.QuerydslPlugin
 
+/**
+ * @author Illya Boyko
+ */
 public class QuerydslCompileTest {
+
     private Project project;
 
     private SourceTask compileTask;
-
 
     public QuerydslCompileTest() {
 	project = ProjectBuilder.builder().build();
@@ -23,12 +41,11 @@ public class QuerydslCompileTest {
 	compileTask = project.tasks.compileQuerydsl as SourceTask
 	compileTask.includes += ['**/entities/*.java']
 	project.evaluate()
-
 	
     }
     
     @Test
-    void tesIncludes() {
+    void testIncludes() {
 	def includes = compileTask.getIncludes()
 	assertThat(compileTask.getIncludes(), hasItem(is('**/entities/*.java')))
     }
