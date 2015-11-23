e0a1008295db276e277d4c414d61e633a04cb136
Holger Stolzenberg <h.stolzenberg@ewerk.com>
2015-11-23 12:13:46 +0100

    The JAXB2 dependency defaults are applied by the plugin itself
    As the plugin also creates a 'jaxb2' configuration, there is still
    the chance to configure the dependencies from the end user
    build script.

--- plugins/jaxb2-plugin/src/main/groovy/com/ewerk/gradle/plugins/Jaxb2Plugin.groovy
+++ plugins/jaxb2-plugin/src/main/groovy/com/ewerk/gradle/plugins/Jaxb2Plugin.groovy
@@ -6,6 +6,7 @@ import com.ewerk.gradle.plugins.tasks.InitJaxb2SourcesDir
 import com.ewerk.gradle.plugins.tasks.XjcTaskConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.internal.reflect.Instantiator
 import org.slf4j.Logger
@@ -20,6 +21,10 @@ class Jaxb2Plugin implements Plugin<Project> {
 
   private static final Logger LOG = LoggerFactory.getLogger(Jaxb2Plugin.class)
 
+  private static final String SLF4J_VERSION = '1.7.13'
+  private static final String JAXB2_VERSION = '0.9.5'
+  private static final String JAXB_VERSION = '2.2.11'
+
   @Override
   void apply(final Project project) {
     // do nothing if plugin is already applied
@@ -29,19 +34,37 @@ class Jaxb2Plugin implements Plugin<Project> {
 
     LOG.info("Applying JAXB2 plugin")
 
-    // apply core 'java' plugin if not present to make 'sourceSets' available
-    if (!project.plugins.hasPlugin(JavaPlugin.class)) {
-      project.plugins.apply(JavaPlugin.class)
-    }
+    applyJavaPlugin(project)
+    addJaxb2Extension(project)
+    addJaxb2Configuration(project)
+    addTasks(project)
+    addJaxbDependencies(project)
 
-    // add 'autoValue' DSL extension
+    // make sure project configuration has finished
+    project.afterEvaluate {
+      addSourceSet(project)
+    }
+  }
+
+  private static Configuration addJaxb2Configuration(Project project) {
+    project.configurations.create('jaxb2')
+  }
+
+  private static void addJaxb2Extension(Project project) {
     project.extensions.create(Jaxb2PluginExtension.NAME, Jaxb2PluginExtension)
     project.extensions.jaxb2.extensions.xjc = project.container(XjcTaskConfig) { String name ->
       return project.gradle.services.get(Instantiator).newInstance(XjcTaskConfig, name)
     }
+  }
 
-    project.configurations.create('jaxb2')
+  private static void applyJavaPlugin(Project project) {
+    // apply core 'java' plugin if not present to make 'sourceSets' available
+    if (!project.plugins.hasPlugin(JavaPlugin.class)) {
+      project.plugins.apply(JavaPlugin.class)
+    }
+  }
 
+  private static void addTasks(Project project) {
     // add new tasks for creating/cleaning the auto-value sources dir
     project.task(type: CleanJaxb2SourcesDir, "cleanJaxb2SourcesDir")
     project.task(type: InitJaxb2SourcesDir, "initJaxb2SourcesDir")
@@ -51,14 +74,19 @@ class Jaxb2Plugin implements Plugin<Project> {
     project.tasks.clean.dependsOn project.tasks.cleanJaxb2SourcesDir
     project.tasks.generateJaxb2Classes.dependsOn project.tasks.initJaxb2SourcesDir
     project.tasks.compileJava.dependsOn project.tasks.generateJaxb2Classes
+  }
 
+  private addJaxbDependencies(Project project) {
     project.dependencies {
-      jaxb2 'org.slf4j:slf4j-simple:1.7.13'
-    }
+      jaxb2 'org.slf4j:slf4j-simple:' + SLF4J_VERSION
 
-    // make sure project configuration has finished
-    project.afterEvaluate {
-      addSourceSet(project)
+      jaxb2 'org.jvnet.jaxb2_commons:jaxb2-basics-runtime:' + JAXB2_VERSION
+      jaxb2 'org.jvnet.jaxb2_commons:jaxb2-basics-ant:' + JAXB2_VERSION
+      jaxb2 'org.jvnet.jaxb2_commons:jaxb2-basics:' + JAXB2_VERSION
+
+      jaxb2 'com.sun.xml.bind:jaxb-core:' + JAXB_VERSION
+      jaxb2 'com.sun.xml.bind:jaxb-xjc:' + JAXB_VERSION
+      jaxb2 'com.sun.xml.bind:jaxb-impl:' + JAXB_VERSION
     }
   }
 
